fdf7c638b7e956f4801fa982cd20d841fcf2579d
Raphaelmoi <raphael.mouly@free.fr>
2024-02-25 15:34:06 +0100

    first commit

--- static/ancien_projet5 avant finger-in-the-nois.raph/public/js/messenger.js
+++
@@ -1,7 +0,0 @@
-//Update the chat every 5 seconds
-function reloadChat () {
-	if ($('#refreshAside') != null) {
-    	$('#refreshAside').load('index.php?action=reload');
-	}
-}
-var timeout = setInterval(reloadChat, 5000);
